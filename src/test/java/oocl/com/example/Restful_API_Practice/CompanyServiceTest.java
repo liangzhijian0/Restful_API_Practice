33586eb530184ac3aa452d37d59d432f0ecaa540
Ocean Liang <657175454@qq.com>
2018-07-24 23:57:35 +0800

    test should_return_one_company_when_get_company_by_id

--- src/test/java/oocl/com/example/Restful_API_Practice/CompanyServiceTest.java
+++ src/test/java/oocl/com/example/Restful_API_Practice/CompanyServiceTest.java
@@ -34,4 +34,22 @@ public class CompanyServiceTest {
 
         assertThat(companyService.getAllCompanies(), is(companyService.allCompanies));
     }
+
+    @Test
+    public void should_return_one_company_when_get_company_by_id() {
+
+        CompanyService companyService = new CompanyService();
+        Employee employee1 = new Employee(1,"sanlai",20,"male",3244);
+        Employee employee2 = new Employee(2,"ocean",21,"female",4554);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(employee1);
+        employees.add(employee2);
+
+        Company company1 = new Company(1,"dab",23,employees);
+        Company company2 = new Company(2,"ocelai",32,employees);
+        companyService.allCompanies.add(company1);
+        companyService.allCompanies.add(company2);
+
+        assertThat(companyService.getCompanyById(1), is(company1));
+    }
 }
