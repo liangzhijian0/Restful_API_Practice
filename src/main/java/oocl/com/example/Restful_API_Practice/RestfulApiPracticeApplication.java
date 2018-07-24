30c9433b7020d7dac286832fcf1588792aa66b0d
Ocean Liang <657175454@qq.com>
2018-07-24 19:12:26 +0800

    init RestfulApiPractice

---
+++ src/main/java/oocl/com/example/Restful_API_Practice/RestfulApiPracticeApplication.java
@@ -0,0 +1,12 @@
+package oocl.com.example.Restful_API_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiPracticeApplication.class, args);
+	}
+}
