3450df7ea955a2a688b005adeb391e28de45dcfc
Ocean Liang <657175454@qq.com>
2018-07-25 00:00:34 +0800

    test should_return_companies_when_get_companies_by_page_and_pageSize

--- src/test/java/oocl/com/example/Restful_API_Practice/CompanyServiceTest.java
+++ src/test/java/oocl/com/example/Restful_API_Practice/CompanyServiceTest.java
@@ -52,4 +52,26 @@ public class CompanyServiceTest {
 
         assertThat(companyService.getCompanyById(1), is(company1));
     }
+
+    @Test
+    public void should_return_companies_when_get_companies_by_page_and_pageSize() {
+
+        CompanyService companyService = new CompanyService();
+        Employee employee1 = new Employee(1,"sanlai",20,"male",3244);
+        Employee employee2 = new Employee(2,"ocean",21,"female",4554);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(employee1);
+        employees.add(employee2);
+
+        Company company1 = new Company(1,"dab",23,employees);
+        Company company2 = new Company(2,"ocelai",32,employees);
+        companyService.allCompanies.add(company1);
+        companyService.allCompanies.add(company2);
+
+        List<Company> result = new ArrayList<>();
+        result.add(company2);
+
+        assertThat(companyService.getCompaniesByPage(2,1), is(result));
+    }
 }
+
